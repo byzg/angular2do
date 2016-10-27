4f4706e45dc2a46146e7f49a79f095cac68e3c7c
byzg <byzg@mail.ru>
2016-10-27 20:02:28 +0300

    8 video done

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,24 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { TodoListComponent } from './todo/todo-list.component'
+import { Todo } from './todo/todo'
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>Angular 2</h1>'
+  selector: 'todo-app',
+  templateUrl: './app/app.component.html',
+  styleUrls: ['./app/app.component.css'],
+  directives: [TodoListComponent]
 })
-export class AppComponent { }
+export class AppComponent {
+  title: string;
+  todos: Todo[];
+
+  constructor() {
+    this.title = 'Angular 2To';
+    this.todos = ['Изучить Angular 2', 'Изучить TypeScript', 'Изучить Node'].map((title)=> new Todo(title) );
+  }
+
+  addTodo(input: HTMLInputElement) {
+    this.todos.push(new Todo(input.value));
+    input.value = '';
+  }
+}
